59ab4cbfaa6d2158edda1475008458760bc1bbf9
k90armstrong <k90armstrong@gmail.com>
2017-10-20 13:26:03 -0600

    start to user model

--- models/user.js
+++ models/user.js
@@ -1,1 +1,13 @@
 // make model to handle database stuff for managing users api keys
+var db = require('../Utilities/db');
+
+// the user object
+var User = function (email, apiKey) {
+    var user = {
+        email: email,
+        apiKey: apiKey,
+        add: function () {
+            // function to add to database
+        }
+    }
+}
